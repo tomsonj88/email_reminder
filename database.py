89d429493df8e05e71fbb5976e43e48e5b22a020
Tomson <tomsonj88@wp.pl>
2022-10-29 17:03:06 +0200

    Add method with funcionality to add new rows to table in database. Resolved [task-5]

--- database.py
+++ database.py
@@ -5,7 +5,8 @@ DATABASE module
 import sqlite3
 import logging
 
-logging.basicConfig(format="%(asctime)s %(name)s %(levelname)s %(message)s", level=logging.INFO)
+logging.basicConfig(format="%(asctime)s %(name)s %(levelname)s %(message)s",
+                    level=logging.INFO)
 
 
 class Database:
@@ -52,3 +53,34 @@ class Database:
         self.cursor.execute(create_table_sql_query)
         logging.info(create_table_sql_query)
         logging.info(f"Table {table_name} was created successfully")
+
+    def add_row(self, table_name: str, records_to_added: list[dict]) -> None:
+        """
+        This method can add row to table in database. Please use it only
+        once or every run this method, change records_to_added param to
+        avoid duplicate rows
+        :param table_name: str
+        :param records_to_added: list[dict], example:
+        records_to_added = [{"email": "for_test_only@gmail.com",
+            "name": "Stefan Muller",
+            "book_title": "Star Wars",
+            "return_at": "2023-01-08"},
+           {"email": "test@gmail.com",
+           "name": "Martin Schmidt",
+            "book_title": "Sherlock Holmes",
+            "return_at": "2021-05-28"}
+           ]
+        :return: None
+        """
+        for element in range(len(records_to_added)):
+            insert_sql_query = f"""
+                INSERT INTO {table_name} (email, name, book_title, return_at) 
+                VALUES (
+                "{records_to_added[element]["email"]}", 
+                "{records_to_added[element]["name"]}", 
+                "{records_to_added[element]["book_title"]}", 
+                "{records_to_added[element]["return_at"]}"
+            )"""
+
+            self.cursor.execute(insert_sql_query)
+            self.connection.commit()
