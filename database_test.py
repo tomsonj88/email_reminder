0574c841521ab95a501f81ff1f63d3b6a8fcb7d4
Tomson <tomsonj88@wp.pl>
2022-11-08 21:02:52 +0100

    Add tests for database class

---
+++ database_test.py
@@ -0,0 +1,68 @@
+""" Tests for Database class """
+from database import Database
+
+table_name = "books"
+database = Database(":memory:")
+database.create_table(table_name)
+records = [{"email": "for_test_only@gmail.com",
+            "name": "Stefan Muller",
+            "book_title": "Star Wars",
+            "return_at": "2021-01-08"},
+           {"email": "test@gmail.com",
+            "name": "Martin Schmidt",
+            "book_title": "Sherlock Holmes",
+            "return_at": "2024-05-28"},
+           {"email": "test2@onet.pl",
+            "name": "Jesus Navas",
+            "book_title": "Rainmaker",
+            "return_at": "2020-03-16"}
+           ]
+database.add_row(table_name, records)
+
+
+def test_check_if_all_row_was_added():
+    """
+    Test counting all positions/rows in name column. It is checking if
+    all rows was added to memory database
+    """
+    count_row_sql_query = f"""SELECT COUNT(name) FROM {table_name}"""
+    counter = database.cursor.execute(count_row_sql_query)
+    result = counter.fetchone()[0]
+    assert result == 3
+
+
+def test_check_method_show_all_rows():
+    """
+    Test checks if method show_all_rows display every row from table
+    in database
+    """
+    show_all_records_sql_query = f"""SELECT * FROM {table_name}"""
+    all_rows = database.cursor.execute(show_all_records_sql_query)
+    result = all_rows.fetchall()
+    assert database.show_all_rows(table_name) == result
+
+
+def test_delete_row():
+    """
+    Test checks if method delete_row can remove row from table
+    in database
+    """
+    database.delete_row(table_name, "Martin Schmidt", "Sherlock Holmes")
+    all_rows = database.show_all_rows(table_name)
+    result = []
+    for element in all_rows:
+        if "Martin Schmidt" in element:
+            result.append(True)
+    assert any(result) is False
+
+
+def test_find_books_debtor():
+    """
+    Test checks method show_who_didnt_back_book. It checks when people
+    should return book (return_at column) and compare it with current
+    timestamp. If date was exceed then should return these records
+    """
+    debtors = database.show_who_didnt_back_book(table_name)
+    debtor_book = (3, 'test2@onet.pl', 'Jesus Navas', 'Rainmaker',
+                   '2020-03-16')
+    assert debtor_book in debtors
